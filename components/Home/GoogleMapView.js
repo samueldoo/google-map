b82be88011eabf7cbdbbd2b3ec74d23c2663cbf9
Alademehin Samuel <alademahinsamuel@yahoo.com>
2023-10-09 14:11:59 +0100

    changes

---
+++ components/Home/GoogleMapView.js
@@ -0,0 +1,62 @@
+
+import { UserLocationContext } from '@/context/UserLocationContext'
+import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api'
+import React, { useContext, useEffect, useState } from 'react'
+import Markers from './Markers'
+import { SelectedBusinessContext } from '@/context/SelectedBusinessContext'
+
+function GoogleMapView({businessList}) {
+  const {userLocation,setUserLocation}=useContext(UserLocationContext)
+  const {selectedBusiness,setSelectedBusiness}=useContext(SelectedBusinessContext)
+  const [map,setMap]=useState();
+
+    const containerStyle={
+        width:'100%',
+        height:'500px',
+        
+    }
+
+    useEffect(()=>{
+      if(map&&selectedBusiness)
+      {
+       map.panTo(selectedBusiness.geometry.location);
+
+      }
+    },[selectedBusiness])
+  return (
+    <div>
+        <LoadScript
+            googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+            mapIds={['1eae3baac059db3a']}
+       >
+            <GoogleMap
+            mapContainerStyle={containerStyle}
+            // center={userLocation}
+           
+             center={
+            !selectedBusiness.name?userLocation:selectedBusiness.geometry.location
+          }
+            options={{mapId:'1eae3baac059db3a'}}
+            zoom={13}
+            onLoad={map=>setMap(map)}
+            >
+              <MarkerF
+                position={userLocation}
+                icon={{
+                  url:'/user-location.png',
+                  scaledSize:{
+                    width:50,
+                    height:50
+                  }
+                }}
+              />
+              {businessList.map((item,index)=>index<=7&&(
+                <Markers business={item} key={index}/>
+              ))}
+            </GoogleMap>
+        </LoadScript>
+    </div>
+  )
+}
+
+export default GoogleMapView
